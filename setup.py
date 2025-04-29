c18b62ee0eb918b2f7b03f11cd6075f823c76f50
yu432 <77671036+yu432@users.noreply.github.com>
2025-04-29 12:09:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -172,8 +172,8 @@ if sys.platform == "win32":
         CC_FLAGS += ["/sdl"]
     else:
         CC_FLAGS += ["/sdl", "/permissive-"]
-#else:
-#    CC_FLAGS += ["-fopenmp", "-Xcompiler"]
+else:
+    CC_FLAGS += ["-fPIC"]
 
 if "darwin" in platform:
     CC_FLAGS += ["-stdlib=libc++", "-std=c++17"]
